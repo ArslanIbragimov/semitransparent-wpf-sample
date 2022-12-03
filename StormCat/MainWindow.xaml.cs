70c4bddb9f2f352999dd014c35d95b66d99c99ef
ArslanIbragimov <117119270+ArslanIbragimov@users.noreply.github.com>
2022-12-03 07:52:03 +0300

    Add files via upload

---
+++ StormCat/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StormCat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Button_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            this.Background = Brushes.DarkGreen;
+        }
+
+        private void Button_MouseEnter_2(object sender, MouseEventArgs e)
+        {
+            this.Background = Brushes.DarkRed;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+
+        private void dockPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+    }
+}
